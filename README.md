0b9532ac538f311ae6266ae85db1105008a0e88a
K dong min <user@MacBook-Pro.local>
2024-09-29 17:15:21 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# S3 life cycle
+
+Terraform 코드로는 아래의 것이 있음
+- 현재 버전, 이전 버전을 Intelligent-tiering로 옮기는 것
+- 만료된 객체 삭제 마커 또는 완료되지 않은 멀티파트 업로드 삭제 
+
+[블로그 링크](https://dingdingmin-back-end-developer.tistory.com/entry/AWS-S3-LifeCycle-%EC%A0%81%EC%9A%A9%EA%B8%B0-with-Terraform)
---
+++ s3.tf
@@ -0,0 +1,30 @@
+resource "aws_s3_bucket" "example" {
+  bucket = "example"
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "example" {
+  bucket = aws_s3_bucket.example.bucket
+
+  rule {
+    id     = "intelligent-tiering"
+    status = "Enabled"
+
+    filter {
+      prefix = "public/"
+    }
+
+    transition {
+      days          = 0
+      storage_class = "INTELLIGENT_TIERING"
+    }
+
+    noncurrent_version_transition {
+      noncurrent_days = 0
+      storage_class   = "INTELLIGENT_TIERING"
+    }
+
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 2
+    }
+  }
+}
